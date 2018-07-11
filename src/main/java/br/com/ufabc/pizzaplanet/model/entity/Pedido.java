81016427d2d32714f33a385a98d4c817999789a2
kaleb22 <kaleb22@hotmail.com>
2018-07-11 18:38:12 -0300

    adicionando getters and setters

--- src/main/java/br/com/ufabc/pizzaplanet/model/entity/Pedido.java
+++ src/main/java/br/com/ufabc/pizzaplanet/model/entity/Pedido.java
@@ -15,7 +15,52 @@ public class Pedido {
     private int id_pagamento;
     private int id_produto;
     private double preco;
-    
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public int getId_pagamento() {
+        return id_pagamento;
+    }
+
+    public void setId_pagamento(int id_pagamento) {
+        this.id_pagamento = id_pagamento;
+    }
+
+    public int getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(int id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }
