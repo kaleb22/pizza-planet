1fd54ead9b7372b84b2c7307c739b95778a40cb9
Pedro Battistini <pedroluiz2a@hotmail.com>
2018-07-12 00:40:37 -0300

    subindo novo html, css, js, imagens, fontes e tudo +

--- src/main/java/br/com/ufabc/pizzaplanet/controller/PizzaPlanetController.java
+++ src/main/java/br/com/ufabc/pizzaplanet/controller/PizzaPlanetController.java
@@ -10,4 +10,29 @@ public class PizzaPlanetController {
     public String main(){
         return "index";
     }
+
+    @RequestMapping(value = "/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @RequestMapping(value = "/cart")
+    public String cart(){
+        return "cart";
+    }
+
+    @RequestMapping(value = "/checkout")
+    public String checkout(){
+        return "checkout";
+    }
+
+    @RequestMapping(value = "/product")
+    public String product(){
+        return "product";
+    }
+
+    @RequestMapping(value = "/categories")
+    public String categories(){
+        return "categories";
+    }
 }
