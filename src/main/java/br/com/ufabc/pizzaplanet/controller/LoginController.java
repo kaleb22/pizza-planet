9438667820100a20174489670bd19516e5828926
Pedro Battistini <pedroluiz2a@hotmail.com>
2018-08-17 01:01:26 -0300

    subindo sessao

--- src/main/java/br/com/ufabc/pizzaplanet/controller/LoginController.java
+++ src/main/java/br/com/ufabc/pizzaplanet/controller/LoginController.java
@@ -4,14 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import br.com.ufabc.pizzaplanet.model.entity.Login;
 import br.com.ufabc.pizzaplanet.model.entity.Cliente;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import br.com.ufabc.pizzaplanet.model.dao.LoginDao;
 import br.com.ufabc.pizzaplanet.model.dao.UserDao;
 import java.util.Base64;
+import javax.servlet.http.*;
 
 @Controller
 public class LoginController {
@@ -23,7 +22,7 @@ public class LoginController {
     UserDao userDao;
 
     @RequestMapping(value = "/loginProcess", method = RequestMethod.POST)
-    public ModelAndView loginProcess(HttpServletRequest request, HttpServletResponse response,
+    public ModelAndView loginProcess(HttpSession session, HttpServletRequest request, HttpServletResponse response,
                                      @RequestParam String cpf, @RequestParam String senha) {
         ModelAndView mav = null;
 
@@ -38,13 +37,12 @@ public class LoginController {
             Cliente userInfo = userDao.findbyCpf(userLogin.getCPF());
 
             mav = new ModelAndView("index");
-            mav.addObject("welcome_username", "Olá, " + userInfo.getNome());
 
+            session.setAttribute("welcome_username", "Olá, " + userInfo.getNome());
         } catch (Exception e) {
             mav = new ModelAndView("login");
             mav.addObject("login_failed", "Usuário e/ou senha não encontrados!");
         }
         return mav;
     }
-
 }
