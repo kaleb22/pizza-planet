4aac034df1e4f5602d648f9ef30b67764fe00cba
kaleb22 <kaleb22@hotmail.com>
2018-08-18 20:34:38 -0300

    alterações tela cardapio

--- src/main/java/br/com/ufabc/pizzaplanet/controller/NavegacaoController.java
+++ src/main/java/br/com/ufabc/pizzaplanet/controller/NavegacaoController.java
@@ -3,13 +3,16 @@ package br.com.ufabc.pizzaplanet.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import br.com.ufabc.pizzaplanet.model.entity.Login;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import br.com.ufabc.pizzaplanet.model.dao.LoginDao;
+
 import javax.persistence.*;
 
 
@@ -17,37 +20,37 @@ import javax.persistence.*;
 public class NavegacaoController {
 
     @RequestMapping(value = "/index")
-    public String main(){
+    public String main() {
         return "index";
     }
 
     @RequestMapping(value = "/cadastro")
-    public String contact(){
+    public String contact() {
         return "cadastro";
     }
 
     @RequestMapping(value = "/cart")
-    public String cart(){
+    public String cart() {
         return "cart";
     }
 
     @RequestMapping(value = "/checkout")
-    public String checkout(){
+    public String checkout() {
         return "checkout";
     }
 
     @RequestMapping(value = "/product")
-    public String product(){
+    public String product() {
         return "product";
     }
 
-    @RequestMapping(value = "/categories")
-    public String categories(){
-        return "categories";
+    @RequestMapping(value = "/cardapio")
+    public String cardapio() {
+        return "cardapio";
     }
 
     @RequestMapping(value = "/login")
-    public String login(){
+    public String login() {
         return "login";
     }
 }
